88a7b106e9d9cd0767cda2aca2dcbef591b5a451
nomelif <theo.friberg@gmail.com>
2014-02-26 14:09:07 +0200

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -3,7 +3,7 @@ public class Bullet extends Blittable{
 public Bullet(){
     this.color = Color.YELLOW;
     this.image = {{true, true}, {true, true}};
-
+    this.InvType = Blittable.BULLET;
     // Inits the bullet to a YELLOW Color and a 4 x 4 size
 
 }

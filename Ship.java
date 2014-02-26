58a962eaafe4ff084641c7486e315dba6c0b49ae
nomelif <theo.friberg@gmail.com>
2014-02-26 14:15:33 +0200

    Create Ship.java

---
+++ Ship.java
@@ -0,0 +1,14 @@
+public class Ship extends Blittable{
+
+public Ship(int[] location){
+    this.x = location[0];
+    this.y = location[1];
+    this.image = {{false, false, true, false, false}, {false, true, true, true, false}, {true, true, true, true, true}};
+    //--#--
+    //-###-
+    //#####
+    this.color = Color.BLUE;
+    this.InvType = Blittable.SHIP;
+}
+
+}

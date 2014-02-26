abdf73efd8ab63706024184654462cf7a1342000
nomelif <theo.friberg@gmail.com>
2014-02-26 13:59:36 +0200

    Update Blittable.java

--- Blittable.java
+++ Blittable.java
@@ -2,8 +2,11 @@ public abstract class Blittable{
 
 protected boolean[][] image;
 protected Color color;
+protected int x;
+protected int y;
 
 // true values in image should be filled with the color defined in the color variable
+//Location of the object is defined by x and y, size by image
 
 public boolean[][] getImage(){
     return(this.image);
@@ -24,3 +27,15 @@ public void setColor(Color color){
 }
 
 // Getters and setters for color
+
+public int[] getLocation(){
+    int[] location = new int[] {x, y};
+    return(location);
+}
+
+public void setLocation(int[] location){
+    this.x = location[0];
+    this.y = location[1];
+}
+
+// Getters and setters for x and y
